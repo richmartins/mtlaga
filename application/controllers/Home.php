c408b149759fd73c20c6bd6d80bccb50765908e8
richmartins <richard.martinstenorio@epfl.ch>
2019-02-06 18:14:26 +0100

    update database config & controllers

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
     public function index() {
-      $this->load->database();
+        $this->load->database();
 
         $data = array(
           'title' => 'Home | MTLAGA',
