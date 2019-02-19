dbfeed639e08e43a7a23a14d16df4d89ba336906
richmartins <richard.martinstenorio@epfl.ch>
2019-02-19 16:44:27 +0100

    [DONE] if connected or not login button(img) work

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,6 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['login/(.+)'] = 'Auth/login/';
+$route['login/(.+)'] = 'auth/login/';
 $route['home'] = 'home';
 $route['info'] = 'home/info';
