191203c0fabaef1696dfb07eced798796b2ee8a5
hadrien <hadry.louis@gmail.com>
2019-03-10 18:28:37 +0100

    [WIP] Added action box to travel details in itinerary page

--- static_design/itineraire.php
+++ static_design/itineraire.php
@@ -26,11 +26,11 @@ include("header.html");
       // Durée trajet
       $train_traject_length = "2 h 6 min";
       // Nombre changement
-      $train_change = "3x Changements";
+      $train_change = "3 Changements";
       // Ville départ
-      $train_departure_city = "Genève";
+      $train_departure_city = "Neuhausen am Rheinfall";
       // Ville départ
-      $train_arrival_city = "Lausanne";
+      $train_arrival_city = "	Neuhausen am Rheinfall";
       // Quai de départ
       $train_departure_platform = "Voie 2";
       // Quai d'arrivée
@@ -59,13 +59,15 @@ include("header.html");
                     <div><p><?= $train_departure ?></p></div>
                     <div><p><b><?= $train_departure_city ?></b></p></div>
                 </div>
-                <div>
+                <div class="itineraire_flex_container_travel_line">
                   <span class="dot dot_start"></span>
+                    <!--
                   <svg height="30" width="200">
                    <g fill="none">
                      <path stroke="red" d="M0 20 l200 0" stroke-width="3" />
                    </g>
                   </svg>
+                  -->
                   <span class="dot dot_end"></span>
                 </div>
                 <div>
@@ -90,36 +92,65 @@ include("header.html");
         <div class="itineraire_flex_container_travel_panel flex_container">
               <!-- Bloc trajet point A vers point B -->
               <div class="itineraire_flex_container_travel_details flex_container">
-                  <div class="itineraire_flex_container_travel_details_hours">
-                      <div class="itineraire_flex_container_travel_details_hours_start">
-                          <p><?= $train_departure ?></p>
+                  <div class="flex_container itineraire_flex_container_travel_details_container">
+                      <div class="itineraire_flex_container_travel_details_hours">
+                          <div class="itineraire_flex_container_travel_details_hours_start">
+                              <p><?= $train_departure ?></p>
+                          </div>
+                          <div class="itineraire_flex_container_travel_details_end">
+                              <p><?= $train_arrival ?></p>
+                          </div>
                       </div>
-                      <div class="itineraire_flex_container_travel_details_end">
-                          <p><?= $train_arrival ?></p>
+                      <div class="itineraire_flex_container_travel_details_line">
+                          <span class="dot dot_start_details"></span>
+                          <svg height="170" width="30">
+                              <g fill="none">
+                                  <path stroke="red" d="M 0 0 L 0 200 " stroke-width="5" />
+                              </g>
+                          </svg>
+                          <span class="dot dot_end_details"></span>
+                      </div>
+                      <div class="itineraire_flex_container_travel_details_box">
+                          <div class="flex_container itineraire_flex_container_travel_details_start">
+                              <p><b><?= $train_departure_city ?></b><?= ", " . $train_departure_platform ?></p>
+                          </div>
+                          <div class="flex_container itineraire_flex_container_travel_details_infos">
+                              <div><i class="fas fa-train fa-lg"></i> <b><?= $train_number ?></b> Direction <b><?= $train_direction ?></b></div>
+                              <div><p><b>42</b> min</p></div>
+                          </div>
+                          <div class="itineraire_flex_container_travel_details_end flex_container">
+                              <p><b><?= $train_arrival_city ?></b><?= ", " . $train_arrival_platform ?></p>
+                          </div>
                       </div>
                   </div>
-                  <div class="itineraire_flex_container_travel_details_line">
-                      <span class="dot dot_start_details"></span>
-                      <svg height="170" width="30">
-                          <g fill="none">
-                              <path stroke="red" d="M 0 0 L 0 200 " stroke-width="5" />
-                          </g>
-                      </svg>
-                      <span class="dot dot_end_details"></span>
-                  </div>
-                  <div class="itineraire_flex_container_travel_details_box">
-                      <div class="flex_container itineraire_flex_container_travel_details_start">
-                          <p><b><?= $train_departure_city ?></b><?= ", " . $train_departure_platform ?></p>
+
+                  <div class="flex_container itineraire_flex_container_travel_action">
+                      <div class="itineraire_flex_container_travel_action_perturbations">
+                          <p><b>Info trafic</b></p>
+                          <div>
+                              <p>Aucun dérangement n'est signalé</p>
+                          </div>
                       </div>
-                      <div class="flex_container itineraire_flex_container_travel_details_infos">
-                          <div><i class="fas fa-train fa-lg"></i> <b><?= $train_number ?></b> Direction <b><?= $train_direction ?></b></div>
-                          <div><p><b>42</b> min</p></div>
+                      <div class="itineraire_flex_container_travel_action_outils">
+                          <p><b>Cette relation</b></p>
+                          <ul>
+                              <div class="flex_container itineraire_flex_container_travel_action_outils_icon">
+                                  <li> Ajouter au favoris</li>
+                                  <i class="fas fa-star"></i>
+                                  <i class="fas fa-long-arrow-alt-right animated fadeInLeft" style="display: none; padding-right: 5px"></i>
+                              </div>
+                              <div class="flex_container itineraire_flex_container_travel_action_outils_icon">
+                                  <li> Afficher sur la carte</li>
+                                  <i class="fas fa-map-marked-alt"></i>
+                                  <i class="fas fa-long-arrow-alt-right animated fadeInLeft" style="display: none; padding-right: 5px"></i>
+                              </div>
+                              <div class="flex_container itineraire_flex_container_travel_action_outils_icon">
+                                  <li> Ajouter au calendrier</li>
+                                  <i class="fas fa-calendar-alt"></i>
+                                  <i class="fas fa-long-arrow-alt-right animated fadeInLeft" style="display: none; padding-right: 5px"></i>
+                              </div>
+                          </ul>
                       </div>
-                      <div class="itineraire_flex_container_travel_details_end flex_container">
-                          <p><b><?= $train_arrival_city ?></b><?= ", " . $train_arrival_platform ?></p>
-                      </div>
-                  </div>
-                  <div style="border: 2px solid green; width: auto">
                   </div>
               </div>
 
@@ -132,36 +163,63 @@ include("header.html");
 
               <!-- Bloc trajet point A vers point B -->
               <div class="itineraire_flex_container_travel_details flex_container">
-                  <div class="itineraire_flex_container_travel_details_hours">
-                      <div class="itineraire_flex_container_travel_details_hours_start">
-                          <p><?= $train_departure ?></p>
+                  <div class="flex_container itineraire_flex_container_travel_details_container">
+                      <div class="itineraire_flex_container_travel_details_hours">
+                          <div class="itineraire_flex_container_travel_details_hours_start">
+                              <p><?= $train_departure ?></p>
+                          </div>
+                          <div class="itineraire_flex_container_travel_details_end">
+                              <p><?= $train_arrival ?></p>
+                          </div>
                       </div>
-                      <div class="itineraire_flex_container_travel_details_end">
-                          <p><?= $train_arrival ?></p>
+                      <div class="itineraire_flex_container_travel_details_line">
+                          <span class="dot dot_start_details"></span>
+                          <svg height="170" width="30">
+                              <g fill="none">
+                                  <path stroke="red" d="M 0 0 L 0 200 " stroke-width="5" />
+                              </g>
+                          </svg>
+                          <span class="dot dot_end_details"></span>
+                      </div>
+                      <div class="itineraire_flex_container_travel_details_box">
+                          <div class="flex_container itineraire_flex_container_travel_details_start">
+                              <p><b><?= $train_departure_city ?></b><?= ", " . $train_departure_platform ?></p>
+                          </div>
+                          <div class="flex_container itineraire_flex_container_travel_details_infos">
+                              <div><i class="fas fa-train fa-lg"></i> <b><?= $train_number ?></b> Direction <b><?= $train_direction ?></b></div>
+                              <div><p><b>42</b> min</p></div>
+                          </div>
+                          <div class="itineraire_flex_container_travel_details_end flex_container">
+                              <p><b><?= $train_arrival_city ?></b><?= ", " . $train_arrival_platform ?></p>
+                          </div>
                       </div>
                   </div>
-                  <div class="itineraire_flex_container_travel_details_line">
-                      <span class="dot dot_start_details"></span>
-                      <svg height="170" width="30">
-                          <g fill="none">
-                              <path stroke="red" d="M 0 0 L 0 200 " stroke-width="5" />
-                          </g>
-                      </svg>
-                      <span class="dot dot_end_details"></span>
-                  </div>
-                  <div class="itineraire_flex_container_travel_details_box">
-                      <div class="flex_container itineraire_flex_container_travel_details_start">
-                          <p><b><?= $train_departure_city ?></b><?= ", " . $train_departure_platform ?></p>
+
+                  <div class="flex_container itineraire_flex_container_travel_action">
+                      <div class="itineraire_flex_container_travel_action_perturbations">
+                          <p><b>Info trafic</b></p>
+                          <p>Aucun dérangement n'est signalé</p>
                       </div>
-                      <div class="flex_container itineraire_flex_container_travel_details_infos">
-                          <div><i class="fas fa-train fa-lg"></i> <b>IR 90</b> Direction <b>Brig</b></div>
-                          <div><p><b>42</b> min</p></div>
+                      <div class="itineraire_flex_container_travel_action_outils">
+                          <p><b>Cette relation</b></p>
+                          <ul>
+                              <div class="flex_container itineraire_flex_container_travel_action_outils_icon">
+                                  <li> Ajouter au favoris</li>
+                                  <i class="fas fa-star"></i>
+                                  <i class="fas fa-long-arrow-alt-right animated fadeInLeft" style="display: none; padding-right: 5px"></i>
+                              </div>
+                              <div class="flex_container itineraire_flex_container_travel_action_outils_icon">
+                                  <li> Afficher sur la carte</li>
+                                  <i class="fas fa-map-marked-alt"></i>
+                                  <i class="fas fa-long-arrow-alt-right animated fadeInLeft" style="display: none; padding-right: 5px"></i>
+                              </div>
+                              <div class="flex_container itineraire_flex_container_travel_action_outils_icon">
+                                  <li> Ajouter au calendrier</li>
+                                  <i class="fas fa-calendar-alt"></i>
+                                  <i class="fas fa-long-arrow-alt-right animated fadeInLeft" style="display: none; padding-right: 5px"></i>
+                              </div>
+                          </ul>
                       </div>
-                      <div class="itineraire_flex_container_travel_details_end flex_container">
-                          <p><b><?= $train_arrival_city ?></b><?= ", " . $train_arrival_platform ?></p>
-                      </div>
-                  </div>
-                  <div style="border: 2px solid green; width: auto">
                   </div>
               </div>
 
@@ -174,36 +232,65 @@ include("header.html");
 
               <!-- Bloc trajet point A vers point B -->
               <div class="itineraire_flex_container_travel_details flex_container">
-                  <div class="itineraire_flex_container_travel_details_hours">
-                      <div class="itineraire_flex_container_travel_details_hours_start">
-                          <p><?= $train_departure ?></p>
+                  <div class="flex_container itineraire_flex_container_travel_details_container">
+                      <div class="itineraire_flex_container_travel_details_hours">
+                          <div class="itineraire_flex_container_travel_details_hours_start">
+                              <p><?= $train_departure ?></p>
+                          </div>
+                          <div class="itineraire_flex_container_travel_details_end">
+                              <p><?= $train_arrival ?></p>
+                          </div>
                       </div>
-                      <div class="itineraire_flex_container_travel_details_end">
-                          <p><?= $train_arrival ?></p>
+                      <div class="itineraire_flex_container_travel_details_line">
+                          <span class="dot dot_start_details"></span>
+                          <svg height="170" width="30">
+                              <g fill="none">
+                                  <path stroke="red" d="M 0 0 L 0 200 " stroke-width="5" />
+                              </g>
+                          </svg>
+                          <span class="dot dot_end_details"></span>
+                      </div>
+                      <div class="itineraire_flex_container_travel_details_box">
+                          <div class="flex_container itineraire_flex_container_travel_details_start">
+                              <p><b><?= $train_departure_city ?></b><?= ", " . $train_departure_platform ?></p>
+                          </div>
+                          <div class="flex_container itineraire_flex_container_travel_details_infos">
+                              <div><i class="fas fa-train fa-lg"></i> <b><?= $train_number ?></b> Direction <b><?= $train_direction ?></b></div>
+                              <div><p><b>42</b> min</p></div>
+                          </div>
+                          <div class="itineraire_flex_container_travel_details_end flex_container">
+                              <p><b><?= $train_arrival_city ?></b><?= ", " . $train_arrival_platform ?></p>
+                          </div>
                       </div>
                   </div>
-                  <div class="itineraire_flex_container_travel_details_line">
-                      <span class="dot dot_start_details"></span>
-                      <svg height="170" width="30">
-                          <g fill="none">
-                              <path stroke="red" d="M 0 0 L 0 200 " stroke-width="5" />
-                          </g>
-                      </svg>
-                      <span class="dot dot_end_details"></span>
-                  </div>
-                  <div class="itineraire_flex_container_travel_details_box">
-                      <div class="flex_container itineraire_flex_container_travel_details_start">
-                          <p><b><?= $train_departure_city ?></b><?= ", " . $train_departure_platform ?></p>
+
+                  <div class="flex_container itineraire_flex_container_travel_action">
+                      <div class="itineraire_flex_container_travel_action_perturbations">
+                          <p><b>Info trafic</b></p>
+                          <div>
+                              <p>Aucun dérangement n'est signalé</p>
+                          </div>
                       </div>
-                      <div class="flex_container itineraire_flex_container_travel_details_infos">
-                          <div><i class="fas fa-train fa-lg"></i> <b>IR 90</b> Direction <b>Brig</b></div>
-                          <div><p><b>42</b> min</p></div>
+                      <div class="itineraire_flex_container_travel_action_outils">
+                          <p><b>Cette relation</b></p>
+                          <ul>
+                              <div class="flex_container itineraire_flex_container_travel_action_outils_icon">
+                                  <li> Ajouter au favoris</li>
+                                  <i class="fas fa-star"></i>
+                                  <i class="fas fa-long-arrow-alt-right animated fadeInLeft" style="display: none; padding-right: 5px"></i>
+                              </div>
+                              <div class="flex_container itineraire_flex_container_travel_action_outils_icon">
+                                  <li> Afficher sur la carte</li>
+                                  <i class="fas fa-map-marked-alt"></i>
+                                  <i class="fas fa-long-arrow-alt-right animated fadeInLeft" style="display: none; padding-right: 5px"></i>
+                              </div>
+                              <div class="flex_container itineraire_flex_container_travel_action_outils_icon">
+                                  <li> Ajouter au calendrier</li>
+                                  <i class="fas fa-calendar-alt"></i>
+                                  <i class="fas fa-long-arrow-alt-right animated fadeInLeft" style="display: none; padding-right: 5px"></i>
+                              </div>
+                          </ul>
                       </div>
-                      <div class="itineraire_flex_container_travel_details_end flex_container">
-                          <p><b><?= $train_arrival_city ?></b><?= ", " . $train_arrival_platform ?></p>
-                      </div>
-                  </div>
-                  <div style="border: 2px solid green; width: auto">
                   </div>
               </div>
 
@@ -228,8 +315,17 @@ include("header.html");
         };
     }
     $(".itineraire_flex_container_travel_rotate").click(function(){
-        $(this).toggleClass("down")  ;
-    })
+        $(this).toggleClass("down");
+    });
+
+    $(".itineraire_flex_container_travel_action_outils_icon").mouseover(function() {
+           $(this).children(":nth-child(2)").hide()
+           $(this).children(":nth-child(3)").show()
+    });
+    $(".itineraire_flex_container_travel_action_outils_icon").mouseleave(function() {
+        $(this).children(":nth-child(2)").show()
+        $(this).children(":nth-child(3)").hide()
+    });
 </script>
 
 <style>
