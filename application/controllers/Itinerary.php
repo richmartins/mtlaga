09371b1ae8f5605c30a8a2cb9fa78322bf6700de
hadrylouis <26306723+hadrylouis@users.noreply.github.com>
2019-03-21 17:54:01 +0100

    [WIP] Opendata GET request CURL

--- application/controllers/Itinerary.php
+++ application/controllers/Itinerary.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Itinerary extends CI_Controller {
-    
+
     var $header_nav;
     var $meta_data;
 
@@ -22,7 +22,7 @@ class Itinerary extends CI_Controller {
             'active' => '',
             'error' => null
         ];
-        
+
         if(isset($_SESSION['email'])){
             $this->meta_data['connected'] = 1;
         }
@@ -32,27 +32,23 @@ class Itinerary extends CI_Controller {
     public function itinerary_process() {
         $departure = $this->input->post("departure_city");
         $arrival = $this->input->post("arrival_city");
-        $result = $this->itineraray_model->get_data_api($departure, $arrival);
-        
-        if($result) {
-            redirect("itinerary");
-        } else {
-            
-        }
+        $result = $this->itinerary_model->get_data_api($departure, $arrival);
+
+        var_dump($result->connections);
     }
 
     /**
-     * render page
+     * Render page
      */
     public function index() {
         $this->meta_data['title'] = "Itinéraire | MTLAGA";
         $this->meta_data['active'] = "Home";
-        
+
         $data = [
             "header_nav_meta_data" => $this->header_nav,
             "meta_data" => $this->meta_data
-        ]
-        
+        ];
+
         $this->load->view("templates/head", $data);
     }
 }
--- application/models/Itinerary_model.php
+++ application/models/Itinerary_model.php
@@ -4,10 +4,45 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Itinerary_model extends CI_Model {
     public function __construct() {
         parent::__construct();
-
     }
 
+    /**
+     * get itinerary data form opendata API
+     * return formated json
+     */
     public function get_data_api($departure, $arrival) {
+      $params = [
+        "from" => $departure,
+        "to" => $arrival,
+        "limit" => 15
+      ];
 
+      $url = "http://transport.opendata.ch/v1/connections?" . http_build_query($params);
+
+      $options= [
+            CURLOPT_URL => $url,
+            CURLOPT_HEADER => false,
+            CURLOPT_FAILONERROR => true,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CUSTOMREQUEST => 'GET'
+      ];
+
+      $curl = curl_init();
+      if(empty($curl)) {
+        die("Une erreur est survenue");
+      }
+
+      curl_setopt_array($curl,$options);
+      $results = json_decode(curl_exec($curl));
+
+      if(curl_errno($curl)){
+          die("Une erreur est survenue");
+      }
+
+      //if($result['connections'])
+
+      curl_close($curl);
+      return $results;
     }
+
 }
